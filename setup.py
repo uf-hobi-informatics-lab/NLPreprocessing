b4efa971b7c8c011956e941e1bd540d7368607c4
bugface <xi@autonomize.ai>
2023-11-06 21:28:07 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ requires = "tqdm"
 
 
 def get_requirements():
-    ret = [x for x in requires.split("\n") if len(x)>0]
+    ret = [x for x in requires.split("\n") if len(x) > 0]
     return ret
 
 
 setuptools.setup(
-    name = 'NLPreprocessing',
-    version = '1.0.0',
-    description = "NLP package for text preprocessing",
+    name="NLPreprocessing",
+    version="1.0.1",
+    description="NLP package for text preprocessing",
     url="https://github.com/uf-hobi-informatics-lab/NLPreprocessing",
     python_requires=">=3.6.0",
     install_requires=get_requirements(),
-    packages=["nlpreprcessing"] + setuptools.find_packages('nlpreprcessing'),
+    packages=["nlpreprcessing"] + setuptools.find_packages("nlpreprcessing"),
     include_package_data=True,
     author="hobi",
 )
